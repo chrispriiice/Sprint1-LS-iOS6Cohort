678ef7a94b2c8c90d7cd04d86c124513a60c67d6
Chris Price <christopher.r.price@gmail.com>
2019-04-26 12:39:18 -0400

    Initial Commit

---
+++ ios-sprint1-challenge-chrisprice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-sprint1-challenge-chrisprice
+//
+//  Created by Chris Price on 4/26/19.
+//  Copyright © 2019 Chris Price. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
