b075fadb24e78f0123d71cf28b6e05a359d0ba66
Chris Price <christopher.r.price@gmail.com>
2019-04-28 19:27:34 -0400

    Added some delegate code that I am not sure the function of. Tried to run in the simluator, and my home screen doesn't have my buttons at the bottom. It's blank.

--- ios-sprint1-challenge-chrisprice/MoviesTableViewController.swift
+++ ios-sprint1-challenge-chrisprice/MoviesTableViewController.swift
@@ -8,8 +8,13 @@
 
 import UIKit
 
+protocol MovieSelectionDelegate: AnyObject {
+    func movieWasSelected(movie: Movie)
+}
+
 class MoviesTableViewController: UITableViewController, MoviePresenter {
     var movieController: MovieController?
+    weak var delegate: MovieSelectionDelegate?
     
 
     override func viewDidLoad() {
@@ -29,59 +34,23 @@ class MoviesTableViewController: UITableViewController, MoviePresenter {
         return movieController?.movies.count ?? 0
     }
 
-    /*
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "movieCell", for: indexPath)
 
-        // Configure the cell...
+       let movie = movieController?.movies[indexPath.row]
 
+        cell.textLabel?.text = movie?.name
+        
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+   
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        guard let movie = movieController?.movies[indexPath.row] else { return }
+        
+        delegate?.movieWasSelected(movie: movie)
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
